6e237c1c1f8996665fde727e53e0fbbc9dd7f929
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-03-30 21:45:53 +0000

    no change

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import './assests/styles/App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./assests/styles/App.css";
 
 // App component
 const App = () => {
-  const [emotion, setEmotion] = useState('neutral');
+  const [emotion, setEmotion] = useState("neutral");
 
   return (
     <Router>
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<Home emotion={emotion} onEmotionDetected={setEmotion} />} />
+          <Route
+            path="/"
+            element={<Home emotion={emotion} onEmotionDetected={setEmotion} />}
+          />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
