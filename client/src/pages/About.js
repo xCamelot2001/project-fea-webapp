3b72efb576f874ceead96bc0f4f5ea739989f4cc
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-04-24 17:55:36 +0100

    Update About page content and add project information in About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,12 +4,16 @@ function About() {
   return (
     <div>
       <p>
-        Welcome to the ExprEssence app! This app generates content based on your emotion and category choice.
+        Welcome to the ExprEssence app!
+      </p>
+      <p>
         This project is made by Hossein Khaneh Masjedi as part of the final year project for the BSc (Hons) in Computing.
+      </p>
+      <p>
         The app uses the face-api.js library to detect your face expression and then generates content based on your emotion and category choice.
       </p>
       <p>
-        If you have problems accessing the camera, please make sure you have given the app permission to access it. The preferred browser is Google Chrome.
+        *** If you have problems accessing the camera, please make sure you have given the app permission to access it. The preferred browser is Google Chrome. ***
       </p>
       <p>
         Please take a moment to fill out the survey to help me improve the app after you're done using it :) Thank you!
