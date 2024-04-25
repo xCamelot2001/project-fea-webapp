107bc903073885284e9353ec91dfdf1703550869
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-04-26 00:35:27 +0100

    Update survey page styling and add new survey questions

--- client/src/features/Survey/SurveyFormat.js
+++ client/src/features/Survey/SurveyFormat.js
@@ -1,71 +1,134 @@
-import React, { useState } from 'react';
-import RadioGroup from './RadioGroup';
-import { submitSurvey } from '../../services/apiService';
-import '../../assests/styles/Survey.css';
+import React, { useState } from "react";
+import RadioGroup from "./RadioGroup";
+import { submitSurvey } from "../../services/apiService";
+import "../../assests/styles/Survey.css";
 
 const susQuestions = [
   {
-    key: 'sus1',
-    label: 'I think that I would like to use this system frequently.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus1",
+    label: "I think that I would like to use this system frequently.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus2',
-    label: 'I found the system unnecessarily complex.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus2",
+    label: "I found the system unnecessarily complex.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus3',
-    label: 'I thought the system was easy to use.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus3",
+    label: "I thought the system was easy to use.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus4',
-    label: 'I think that I would need the support of a technical person to be able to use this system.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus4",
+    label:
+      "I think that I would need the support of a technical person to be able to use this system.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus5',
-    label: 'I found the various functions in this system were well integrated.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus5",
+    label: "I found the various functions in this system were well integrated.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus6',
-    label: 'I thought there was too much inconsistency in this system.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus6",
+    label: "I thought there was too much inconsistency in this system.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus7',
-    label: 'I would imagine that most people would learn to use this system very quickly.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus7",
+    label:
+      "I would imagine that most people would learn to use this system very quickly.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus8',
-    label: 'I found the system very cumbersome to use.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus8",
+    label: "I found the system very cumbersome to use.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus9',
-    label: 'I felt very confident using the system.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus9",
+    label: "I felt very confident using the system.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
   {
-    key: 'sus10',
-    label: 'I needed to learn a lot of things before I could get going with this system.',
-    options: ['Strongly agree', 'Agree', 'Neutral', 'Disagree', 'Strongly disagree']
+    key: "sus10",
+    label:
+      "I needed to learn a lot of things before I could get going with this system.",
+    options: [
+      "Strongly agree",
+      "Agree",
+      "Neutral",
+      "Disagree",
+      "Strongly disagree",
+    ],
   },
 ];
 
 const qualitativeQuestions = [
   {
-    key: 'qualitative1',
-    label: 'What did you like most about using our website?',
-    type: 'text',
+    key: "qualitative1",
+    label: "What did you like most about using our website?",
+    type: "text",
   },
   {
-    key: 'qualitative2',
-    label: 'What improvements would you suggest?',
-    type: 'text',
+    key: "qualitative2",
+    label: "What improvements would you suggest?",
+    type: "text",
   },
 ];
 
@@ -74,11 +137,11 @@ function SurveyFormat() {
   const [submitting, setSubmitting] = useState(false);
 
   const handleOptionChange = (question, option) => {
-    setAnswers(prevAnswers => ({ ...prevAnswers, [question]: option }));
+    setAnswers((prevAnswers) => ({ ...prevAnswers, [question]: option }));
   };
 
   const handleInputChange = (question, text) => {
-    setAnswers(prevAnswers => ({ ...prevAnswers, [question]: text }));
+    setAnswers((prevAnswers) => ({ ...prevAnswers, [question]: text }));
   };
 
   const handleSubmit = async (event) => {
@@ -86,27 +149,27 @@ function SurveyFormat() {
     setSubmitting(true);
     // Map SUS responses to numerical values
     const mappedAnswers = Object.keys(answers).reduce((acc, key) => {
-    if (key.includes('sus')) {
-      const valueMap = {
-        'Strongly agree': 5,
-        'Agree': 4,
-        'Neutral': 3,
-        'Disagree': 2,
-        'Strongly disagree': 1
-      };
-      acc[key] = valueMap[answers[key]];
-    } else {
-      acc[key] = answers[key];
-    }
-    return acc;
-  }, {});
+      if (key.includes("sus")) {
+        const valueMap = {
+          "Strongly agree": 5,
+          Agree: 4,
+          Neutral: 3,
+          Disagree: 2,
+          "Strongly disagree": 1,
+        };
+        acc[key] = valueMap[answers[key]];
+      } else {
+        acc[key] = answers[key];
+      }
+      return acc;
+    }, {});
     try {
       await submitSurvey(mappedAnswers);
       setAnswers({});
       setSubmitting(false);
-      alert('Survey submitted successfully!');
+      alert("Survey submitted successfully!");
     } catch (error) {
-      alert('Failed to submit survey.');
+      alert("Failed to submit survey.");
       setSubmitting(false);
     }
   };
@@ -114,31 +177,38 @@ function SurveyFormat() {
   return (
     <div className="survey-container">
       <h1 className="survey-header">Survey Form</h1>
-      <p className="survey-intro">Your feedback is crucial to us. The survey will take a few minutes and your answers will remain confidential.</p>
+      <p className="survey-intro">
+        Your feedback is crucial to us. The survey will take a few minutes and
+        your answers will remain confidential.
+      </p>
       <form onSubmit={handleSubmit} className="survey-form">
         {susQuestions.map((question) => (
-          <RadioGroup
-            key={question.key}
-            questionKey={question.key}
-            questionLabel={question.label}
-            options={question.options}
-            value={answers[question.key]}
-            onChange={handleOptionChange}
-          />
+          <div key={question.key} className="survey-question">
+            <RadioGroup
+              questionKey={question.key}
+              questionLabel={question.label}
+              options={question.options}
+              value={answers[question.key]}
+              onChange={handleOptionChange}
+            />
+          </div>
         ))}
         {qualitativeQuestions.map((question) => (
-          <div key={question.key} className="survey-question-text">
+          <div
+            key={question.key}
+            className="survey-question survey-question-text"
+          >
             <label htmlFor={question.key}>{question.label}</label>
             <textarea
               id={question.key}
-              value={answers[question.key] || ''}
+              value={answers[question.key] || ""}
               onChange={(e) => handleInputChange(question.key, e.target.value)}
               className="feedback-textarea"
             />
           </div>
         ))}
         <button type="submit" className="submit-button" disabled={submitting}>
-          {submitting ? 'Submitting...' : 'Submit'}
+          {submitting ? "Submitting..." : "Submit"}
         </button>
       </form>
     </div>
